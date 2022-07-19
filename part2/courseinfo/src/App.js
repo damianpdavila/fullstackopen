ef11fb7ab2ebbc67b6d9bf3ebd62465461784734
Damian Davila <damian@moventisusa.com>
2022-07-19 10:18:12 -0400

    Exercise 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,25 +1,4 @@
-
-const Header = ({ course }) => <h1>{course}</h1>
-
-const Total = ({ sum }) => <p>Total of {sum} exercises</p>
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => 
-  <>
-    { parts.map((part) => <Part key={part.id} part={part} />) }
-  </>
-
-const Course = ({course}) => 
-  <>
-    <Header course={course.name} />
-    <Content parts={course.parts} />
-    <Total sum={course.parts.reduce((prev, curr) => prev + curr.exercises, 0 ) } />
-  </>
-
+import Course from './Course';
 
 const App = () => {
 

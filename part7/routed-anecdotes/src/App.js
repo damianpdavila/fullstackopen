38db19022b6bf903d3cd8140b4111b9a09042d59
Damian Davila <damian@moventisusa.com>
2022-12-13 11:50:53 -0500

    Exercise 7.4

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -7,6 +7,7 @@ import {
   useMatch,
   useNavigate,
 } from "react-router-dom";
+import  { useField } from './hooks';
 
 const Menu = () => {
   const padding = {
@@ -102,17 +103,19 @@ const Footer = () => (
 );
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState("");
-  const [author, setAuthor] = useState("");
-  const [info, setInfo] = useState("");
+  const content = useField("text");
+  const author = useField("text");
+  const info = useField("text");
+  // const [author, setAuthor] = useState("");
+  // const [info, setInfo] = useState("");
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     });
     navigate('/');
@@ -124,27 +127,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input {...content} />
         </div>
         <div>
           author
-          <input
-            name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input
-            name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
@@ -211,10 +202,8 @@ const App = () => {
         <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
         <Route path="/create" element={<CreateNew addNew={addNew} />} />
         <Route path="/about" element={<About />} />
-        <Route
-          path="/anecdotes/:id"
-          element={<Anecdote anecdote={anecdote} />}
-        />
+        <Route path="/anecdotes/:id" element={<Anecdote anecdote={anecdote} />}
+      />
       </Routes>
 
       <div>
